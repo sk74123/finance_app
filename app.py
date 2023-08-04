4b1ece8e032b9ed9d528b9e9cfe1925877de386e
accel33 <accelwtf@gmail.com>
2023-08-04 11:59:24 -0500

    second commit

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 import json
@@ -13,7 +12,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 db.init_app(app)
 
 
-#DB Model for User table
+# DB Model for User table
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
@@ -21,10 +20,14 @@ class User(db.Model):
     password = db.Column(db.String(50), nullable=False)
     data_created = db.Column(db.DateTime, default=datetime.utcnow)
 
+@app.route('/user',methods=['GET','POST','PUT','DELETE'])
+def getUser():
+    if request.method == 'GET':
+        user = User.query.all()
+        print(user)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
-
     if request.method == 'GET':
         return "Hello World!!"
     if request.method == 'POST':
@@ -40,46 +43,3 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(debug=True)
-=======
-from flask import Flask, request, render_template
-from flask_sqlalchemy import SQLAlchemy
-import json
-from datetime import datetime
-
-# Creating instance of SQLalchemy
-db = SQLAlchemy()
-
-#Creating instance of Flask
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
-db.init_app(app)
-
-
-#DB Model for User table
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)
-    email = db.Column(db.String)
-    password = db.Column(db.String(50), nullable=False)
-    data_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-
-@app.route('/', methods=['GET', 'POST'])
-def hello_world():
-
-    if request.method == 'GET':
-        return "Hello World!!"
-    if request.method == 'POST':
-        print(request)
-        print(dir(request))
-        print(request.body)
-        error = {
-            'msg': ' POST is not allowed'
-        }
-        error = json.dumps(error)
-        return error
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
->>>>>>> f081fb340c455235d034c177d5c3b21010163168
